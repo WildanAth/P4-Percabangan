c64d12b0b394eec5d58b127ecbd414467f838cb4
Wildan Ath <32089227+WildanAth@users.noreply.github.com>
2017-10-01 18:55:54 +0700

    Add files via upload

---
+++ DemoIf.java
@@ -0,0 +1,9 @@
+public class DemoIf {
+	public static void main(String[] args) {
+		String nim =  "161530021";
+		
+		if(nim == "161530021") {
+			System.out.println("Nama anda Wildan Agung");
+		}
+	}
+}
